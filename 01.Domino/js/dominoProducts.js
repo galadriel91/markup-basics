eb6096a8cd582119013cbf3c139309870fc3563c
GalaDriel <g_ala@naver.com>
2023-04-28 19:13:33 +0900

    Add: Main - MainProducts

---
+++ 01.Domino/js/dominoProducts.js
@@ -0,0 +1,15 @@
+(()=>{
+    $('.mainProductVisual').bxSlider({
+        auto: true,
+        slideWidth:278,
+        slideMargin: 29,
+        speed:1000,
+        minSlides:1,
+        maxSlides:4,
+        moveSlides: 1,
+        autoHover : true,
+        responsive : true,
+        pager: false,
+        stopAutoOnClick: false,
+    });
+})()
