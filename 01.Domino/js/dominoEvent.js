dd9735ca81106d4fcf67bdf1d4df4dee719809f7
GalaDriel <g_ala@naver.com>
2023-04-28 17:55:36 +0900

    Add: Main - MainEventWrap

---
+++ 01.Domino/js/dominoEvent.js
@@ -0,0 +1,11 @@
+const main = new Vue({
+    el:'#main',
+    data:{
+        eventArray:[
+            { src:"./images/main_card1.png", alt:"회원가입 즉시 20% 할인" },
+            { src:"./images/main_card2.gif", alt:"도미노 매니아만의 혜택보기" },
+            { src:"./images/main_card3.png", alt:"희망나눔 캠페인 20%할인" },
+            { src:"./images/main_card4.png", alt:"나만의 피자 먹고싶은 그날 !" }
+        ],
+    },
+})
