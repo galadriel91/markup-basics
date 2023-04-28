dad1aa33a317764e145766879ee4e9cd4be85789
GalaDriel <g_ala@naver.com>
2023-04-28 22:53:34 +0900

    Update: dominoHeader - HandleFixedScroll

---
+++ 01.Domino/js/dominoHeader.js
@@ -0,0 +1,33 @@
+(()=>{
+    // //전체메뉴 버튼 토글 이벤트
+    // const wholeMenuBtn = document.querySelector('.wholeBtn');
+    // const wholeContainer = document.querySelector('.headerBottomWrapWholeMenu');
+    // const open = "open" ;
+    // const showing = "showing"
+
+    // const btnToggle = () => {
+    //     wholeMenuBtn.classList.toggle(open);
+    //     wholeContainer.classList.toggle(showing);
+    // }
+
+    // wholeMenuBtn.addEventListener('click' , btnToggle)
+    // //전체메뉴 버튼 토글 이벤트 끝
+
+    // 헤더 Fixed 이벤트
+    const headerMiddle = document.querySelector('.headerMiddle')
+    const headerBottom = document.querySelector('.headerBottom')
+    const middleFixed = "middleFixed";
+    const fixed = "fixed";
+
+    const handleFixedScroll = () => {
+        if(window.scrollY >= 150){
+            headerMiddle.classList.add(middleFixed);
+            headerBottom.classList.add(fixed);
+        }else{
+            headerMiddle.classList.remove(middleFixed);
+            headerBottom.classList.remove(fixed);
+        }
+    }
+
+    window.addEventListener('scroll' , handleFixedScroll);
+})()
--- 01.Domino/js/dominoVisual.js
+++ 01.Domino/js/dominoVisual.js
@@ -1,26 +1,28 @@
-const slider = $('.visual').bxSlider({
-    auto: true,
-    speed:1000,
-    autoHover : true,
-    responsive : true,
-    pager: true,
-    pagerType: 'short',
-    stopAutoOnClick: false,
-});
-
-const prevBtn = $('.bx-prev')
-prevBtn.on('click' , ()=> {
-    slider.stopAuto(false)
-    timeout = setTimeout(()=>{
-        slider.startAuto(false)
-    },3000)
-})
-
-
-const nextBtn = $('.bx-next')
-nextBtn.on('click' , ()=> {
-    slider.stopAuto(false)
-    timeout = setTimeout(()=>{
-        slider.startAuto(false)
-    },3000)
-})
+(()=>{
+    const slider = $('.visual').bxSlider({
+        auto: true,
+        speed:1000,
+        autoHover : true,
+        responsive : true,
+        pager: true,
+        pagerType: 'short',
+        stopAutoOnClick: false,
+    });
+    
+    const prevBtn = $('.bx-prev')
+    prevBtn.on('click' , ()=> {
+        slider.stopAuto(false)
+        timeout = setTimeout(()=>{
+            slider.startAuto(false)
+        },3000)
+    })
+    
+    
+    const nextBtn = $('.bx-next')
+    nextBtn.on('click' , ()=> {
+        slider.stopAuto(false)
+        timeout = setTimeout(()=>{
+            slider.startAuto(false)
+        },3000)
+    })
+})()
