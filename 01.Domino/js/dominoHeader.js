24d94d3612b0939c07f73ee57258a78d4b375eba
GalaDriel <g_ala@naver.com>
2023-04-29 21:58:00 +0900

    Add: dominoHeader.js

--- 01.Domino/js/dominoHeader.js
+++ 01.Domino/js/dominoHeader.js
@@ -1,21 +1,23 @@
 (()=>{
     // //전체메뉴 버튼 토글 이벤트
-    // const wholeMenuBtn = document.querySelector('.wholeBtn');
-    // const wholeContainer = document.querySelector('.headerBottomWrapWholeMenu');
-    // const open = "open" ;
-    // const showing = "showing"
+    const wholeMenuBtn = document.querySelector('.wholeBtn');
+    const wholeContainer = document.querySelector('.headerWholeMenu');
+    const open = "open" ;
+    const showing = "showing"
 
-    // const btnToggle = () => {
-    //     wholeMenuBtn.classList.toggle(open);
-    //     wholeContainer.classList.toggle(showing);
-    // }
+    const btnToggle = () => {
+        wholeMenuBtn.classList.toggle(open);
+        wholeContainer.classList.toggle(showing);
+    }
 
-    // wholeMenuBtn.addEventListener('click' , btnToggle)
+    wholeMenuBtn.addEventListener('click' , btnToggle)
     // //전체메뉴 버튼 토글 이벤트 끝
 
+    
     // 헤더 Fixed 이벤트
     const headerMiddle = document.querySelector('.headerMiddle')
     const headerBottom = document.querySelector('.headerBottom')
+    
     const middleFixed = "middleFixed";
     const fixed = "fixed";
 
