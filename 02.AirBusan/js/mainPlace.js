1c275fb7a1db133a3e15f224f37b0b0ee576479d
GalaDriel <g_ala@naver.com>
2023-05-03 18:45:28 +0900

    Add: Main - MainPlace

--- 02.AirBusan/js/mainPlace.js
+++ 02.AirBusan/js/mainPlace.js
@@ -1,24 +1,26 @@
-$('.mainPlaceSlider').slick({
-    autoplay: true,
-    autoplaySpeed: 2500,
-    arrows:true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    accessibility:false,
-    pauseOnDotsHover:true,
-});
-
-const btns = document.querySelectorAll('.mainPlace .slick-arrow')
-
-btns.forEach((item)=>{
-    item.addEventListener('click' , ()=>{
-        $('.mainPlace').slick('slickPause');
-        setTimeout(()=>{
-            $('.mainPlace').slick('slickPlay');
-        },1000)
+(()=>{
+    $('.mainPlaceSlider').slick({
+        autoplay: false,
+        autoplaySpeed: 2500,
+        arrows:true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        accessibility:true,
+        pauseOnDotsHover:true,
+    });
+    
+    const btns = document.querySelectorAll('.mainPlace .slick-arrow')
+    
+    btns.forEach((item)=>{
+        item.addEventListener('click' , ()=>{
+            $('.mainPlace').slick('slickPause');
+            setTimeout(()=>{
+                $('.mainPlace').slick('slickPlay');
+            },1000)
+        })
     })
-})
-
-$('.mainPlace').on('touchstart', e => {
-    $('.mainPlace').slick('slickPlay');
-});
+    
+    $('.mainPlace').on('touchstart', e => {
+        $('.mainPlace').slick('slickPlay');
+    });
+})();
