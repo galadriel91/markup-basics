1d5f2b5294322f4ae76b47d5acd1d2c4f32f2d9d
GalaDriel <g_ala@naver.com>
2023-05-03 15:24:59 +0900

    Update: Apply slick & clean code

--- 01.Domino/js/dominoProducts.js
+++ 01.Domino/js/dominoProducts.js
@@ -1,5 +1,5 @@
 (()=>{
-    $('.mainProductVisual').bxSlider({
+    const slider = $('.mainProductVisual').bxSlider({
         auto: true,
         slideWidth:278,
         slideMargin: 29,
@@ -9,7 +9,32 @@
         moveSlides: 1,
         autoHover : true,
         responsive : true,
-        pager: false,
         stopAutoOnClick: false,
+        pager: false,
+
+        onSliderLoad: function(){
+            // 클론 슬라이드 탭 방지
+            $(".bx-clone").find("a").prop("tabIndex","-1");
+        },
+    
+        onSlideAfter: function(){
+            // 아리아 히든 옵션이 있을경우 탭 방지
+            $(".mainProductVisual").children("li").each(function(){
+                if($(this).attr("aria-hidden") == "false"){
+                    $(this).find("a").attr("tabIndex","0");
+                }else{
+                    $(this).find("a").attr("tabIndex","-1");
+                }
+            });
+        }
+    });
+    $('.mainProductVisual a').focusin(function () {
+        slider.stopAuto();
+    });
+    $('.mainProductVisual a').focusout(function () {
+        slider.goToNextSlide()
+    });
+    $('.bx-next').focusout(function () {
+        slider.startAuto()
     });
 })()
--- 01.Domino/js/dominoVisual.js
+++ 01.Domino/js/dominoVisual.js
@@ -1,49 +1,33 @@
 (()=>{
-    const slider = $('.visual').bxSlider({
-        auto: true,
-        speed:1000,
-        autoHover : true,
-        responsive : true,
-        pager: true,
-        pagerType: 'short',
-        stopAutoOnClick: false,
-        keyboardEnabled:true,
-
-        onSliderLoad: function(){
-            // 클론 슬라이드 탭 방지
-            $(".bx-clone").find("a").prop("tabIndex","-1");
-        },
-
-        onSlideAfter: function(){
-            // 아리아 히든 옵션이 있을경우 탭 방지
-            $(".visual").children("li").each(function(){
-                if($(this).attr("aria-hidden") == "false"){
-                    $(this).find("a").attr("tabIndex","0");
-                }else{
-                    $(this).find("a").attr("tabIndex","-1");
-                }
-            });
-        }
+    const visual = $('.visual').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3500,
+        accessibility:true,
+        arrows: true,
     });
-    
-    const prevBtn = $('.bx-prev')
-    prevBtn.on('click' , ()=> {
-        slider.stopAuto(false)
-        timeout = setTimeout(()=>{
-            slider.startAuto(false)
-        },3000)
+    const btns = document.querySelectorAll('.slick-arrow')
+    const dots = document.querySelectorAll('.slick-dots button')
+
+    btns.forEach((item)=>{
+        item.addEventListener('click' , ()=>{
+            visual.slick('slickPause');
+            setTimeout(()=>{
+                visual.slick('slickPlay');
+            },1000)
+        })
     })
-    
-    
-    const nextBtn = $('.bx-next')
-    nextBtn.on('click' , ()=> {
-        slider.stopAuto(false)
-        timeout = setTimeout(()=>{
-            slider.startAuto(false)
-        },3000)
+    dots.forEach((item)=>{
+        item.addEventListener('click' , ()=>{
+            visual.slick('slickPause');
+            setTimeout(()=>{
+                visual.slick('slickPlay');
+            },1000)
+        })
     })
 
-    $('.visual a').focusin(function () {
-        slider.stopAuto();
+    visual.on('touchstart', e => {
+        visual.slick('slickPlay');
     });
 })()
