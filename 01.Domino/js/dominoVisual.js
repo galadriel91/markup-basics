2e77a089b241311a1d66147534b87f803947f5ce
GalaDriel <g_ala@naver.com>
2023-05-03 10:02:39 +0900

    Update: Replace with appropriate tags

--- 01.Domino/js/dominoVisual.js
+++ 01.Domino/js/dominoVisual.js
@@ -7,6 +7,23 @@
         pager: true,
         pagerType: 'short',
         stopAutoOnClick: false,
+        keyboardEnabled:true,
+
+        onSliderLoad: function(){
+            // 클론 슬라이드 탭 방지
+            $(".bx-clone").find("a").prop("tabIndex","-1");
+        },
+
+        onSlideAfter: function(){
+            // 아리아 히든 옵션이 있을경우 탭 방지
+            $(".visual").children("li").each(function(){
+                if($(this).attr("aria-hidden") == "false"){
+                    $(this).find("a").attr("tabIndex","0");
+                }else{
+                    $(this).find("a").attr("tabIndex","-1");
+                }
+            });
+        }
     });
     
     const prevBtn = $('.bx-prev')
@@ -25,4 +42,8 @@
             slider.startAuto(false)
         },3000)
     })
+
+    $('.visual a').focusin(function () {
+        slider.stopAuto();
+    });
 })()
