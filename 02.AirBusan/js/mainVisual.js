ee7f0cf13a51b448896586a8bea96f1c99af9069
GalaDriel <g_ala@naver.com>
2023-05-03 18:58:02 +0900

    Add: Main - MainEvent

--- 02.AirBusan/js/mainVisual.js
+++ 02.AirBusan/js/mainVisual.js
@@ -1,23 +1,25 @@
-$('.mainSlide').slick({
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    arrows:false,
-    accessibility:false,
-    pauseOnDotsHover:true
-});
-
-const dots = document.querySelectorAll('.slick-dots button')
-
-dots.forEach((item)=>{
-    item.addEventListener('click' , ()=>{
-        $('.mainSlide').slick('slickPause');
-        setTimeout(()=>{
-            $('.mainSlide').slick('slickPlay');
-        },1000)
+(()=>{
+    $('.mainSlide').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows:false,
+        accessibility:true,
+        pauseOnDotsHover:true
+    });
+    
+    const dots = document.querySelectorAll('.slick-dots button')
+    
+    dots.forEach((item)=>{
+        item.addEventListener('click' , ()=>{
+            $('.mainSlide').slick('slickPause');
+            setTimeout(()=>{
+                $('.mainSlide').slick('slickPlay');
+            },1000)
+        })
     })
-})
-
-$('.mainSlide').on('touchstart', e => {
-    $('.mainSlide').slick('slickPlay');
-});
+    
+    $('.mainSlide').on('touchstart', e => {
+        $('.mainSlide').slick('slickPlay');
+    });
+})()
