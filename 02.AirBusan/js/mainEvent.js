a8896f0cdf82b7baa3c5f812f0981867fe4ce0db
GalaDriel <g_ala@naver.com>
2023-05-04 14:17:09 +0900

    Update: Define mainVisual arrow

--- 02.AirBusan/js/mainEvent.js
+++ 02.AirBusan/js/mainEvent.js
@@ -1,24 +1,27 @@
-$('.mainEventSlide').slick({
-    autoplay: true,
-    autoplaySpeed: 2500,
-    arrows:true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    accessibility:true,
-    pauseOnDotsHover:true,
-});
-
-const eventBtns = document.querySelectorAll('.mainEventSlide .slick-arrow')
-
-eventBtns.forEach((item)=>{
-    item.addEventListener('click' , ()=>{
-        $('.mainEventSlide').slick('slickPause');
-        setTimeout(()=>{
-            $('.mainEventSlide').slick('slickPlay');
-        },1000)
+(()=>{
+    $('.mainEventSlide').slick({
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows:true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        accessibility:true,
+        pauseOnDotsHover:true,
+    });
+    
+    const eventBtns = document.querySelectorAll('.mainEventSlide .slick-arrow')
+    
+    eventBtns.forEach((item)=>{
+        item.addEventListener('click' , ()=>{
+            $('.mainEventSlide').slick('slickPause');
+            setTimeout(()=>{
+                $('.mainEventSlide').slick('slickPlay');
+            },1000)
+        })
     })
-})
-
-$('.mainEventSlide').on('touchstart', e => {
-    $('.mainEventSlide').slick('slickPlay');
-});
+    
+    $('.mainEventSlide').on('touchstart', e => {
+        $('.mainEventSlide').slick('slickPlay');
+    });
+    
+})();
--- 02.AirBusan/js/mainVisual.js
+++ 02.AirBusan/js/mainVisual.js
@@ -3,13 +3,23 @@
         dots: true,
         autoplay: true,
         autoplaySpeed: 2500,
-        arrows:false,
+        arrows:true,
         accessibility:true,
         pauseOnDotsHover:true
     });
     
+    const btns = document.querySelectorAll('.slick-arrow')
     const dots = document.querySelectorAll('.slick-dots button')
-    
+
+    btns.forEach((item)=>{
+        item.addEventListener('click' , ()=>{
+            $('.mainSlide').slick('slickPause');
+            setTimeout(()=>{
+                $('.mainSlide').slick('slickPlay');
+            },1000)
+        })
+    })
+
     dots.forEach((item)=>{
         item.addEventListener('click' , ()=>{
             $('.mainSlide').slick('slickPause');
