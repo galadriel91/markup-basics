d59174f1c4528a7f66ccbdb700349f820886da15
GalaDriel <g_ala@naver.com>
2023-05-05 11:18:24 +0900

    Add: Main - MainMenu

---
+++ 04.Hollys/js/hollysMenu.js
@@ -0,0 +1,44 @@
+(()=>{
+    $('.menuSlide').slick({
+        accessibility : true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        infinite:true,
+        arrows:false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1150,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 830,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 401,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+        ]
+    });
+    
+    $('.menuSlide').on('touchstart', e => {
+        $('.menuSlide').slick('slickPlay');
+    });
+})()
