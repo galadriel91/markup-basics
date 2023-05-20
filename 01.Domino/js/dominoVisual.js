9ae3c787530944bea05dd260539d4c74e30fd07f
GalaDriel <g_ala@naver.com>
2023-05-20 20:36:24 +0900

    Update: Define StopButton in Visual

--- 01.Domino/js/dominoVisual.js
+++ 01.Domino/js/dominoVisual.js
@@ -9,25 +9,37 @@
     });
     const btns = document.querySelectorAll('.slick-arrow')
     const dots = document.querySelectorAll('.slick-dots button')
+    const stopBtn = document.querySelector('.stop')
+
+
+    const stopSlide = () => {
+        if(stopBtn.classList.contains('xi-angle-right-min')){
+            stopBtn.classList.replace('xi-angle-right-min', 'xi-pause')
+        }
+        visual.slick('slickPause');
+        setTimeout(()=>{
+            visual.slick('slickPlay');
+        },1000)
+    }
 
     btns.forEach((item)=>{
-        item.addEventListener('click' , ()=>{
-            visual.slick('slickPause');
-            setTimeout(()=>{
-                visual.slick('slickPlay');
-            },1000)
-        })
+        item.addEventListener('click' , stopSlide)
     })
     dots.forEach((item)=>{
-        item.addEventListener('click' , ()=>{
-            visual.slick('slickPause');
-            setTimeout(()=>{
-                visual.slick('slickPlay');
-            },1000)
-        })
+        item.addEventListener('click' , stopSlide)
     })
 
     visual.on('touchstart', e => {
         visual.slick('slickPlay');
     });
+
+    stopBtn.addEventListener('click', ()=>{
+        if(stopBtn.classList.contains('xi-pause')){
+            visual.slick('slickPause');
+            stopBtn.classList.replace('xi-pause' , 'xi-angle-right-min')
+        }else{
+            visual.slick('slickPlay');
+            stopBtn.classList.replace('xi-angle-right-min', 'xi-pause')
+        }
+    })
 })()
